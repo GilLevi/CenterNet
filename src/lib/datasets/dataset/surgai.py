f85e1db2db38b03127cfd752809f732844dc89a0
Gil Levi <gil.levi100@gmail.com>
2022-07-14 20:26:47 +0100

    finished first draft of surgai dataset

--- src/lib/datasets/dataset/surgai.py
+++ src/lib/datasets/dataset/surgai.py
@@ -15,7 +15,7 @@ class SurgAI(data.Dataset):
 
   mean = np.array([0.36078363, 0.2696714 , 0.34761672],
                    dtype=np.float32).reshape(1, 1, 3)
-  std  = np.array([0.01912308, 0.01850544, 0.0194903],
+  std = np.array([0.01912308, 0.01850544, 0.0194903],
                    dtype=np.float32).reshape(1, 1, 3)
 
   def __init__(self, opt, split):
@@ -25,53 +25,23 @@ class SurgAI(data.Dataset):
     # self.data_dir = os.path.join(opt.data_dir, 'coco')
     if split == 'train':
       self.img_dir = os.path.join(self.data_dir, 'train_vid3')
-      self.annot_path = os.path.join(self.data_dir, 'annotations')
+      self.annot_path = os.path.join(self.data_dir, 'annotations', 'train_vid3.json')
     elif split == 'val':
       self.img_dir = os.path.join(self.data_dir, 'val_vid4')
+      self.annot_path = os.path.join(self.data_dir, 'annotations', 'val_vid4.json')
     elif split == 'test':
       self.img_dir = os.path.join(self.data_dir, 'test_vid1')
+      self.annot_path = os.path.join(self.data_dir, 'annotations', 'test_vid1.json')
 
-    if split == 'test':
-      self.annot_path = os.path.join(
-          self.data_dir, 'annotations', 
-          'image_info_test-dev2017.json').format(split)
-    else:
-      if opt.task == 'exdet':
-        self.annot_path = os.path.join(
-          self.data_dir, 'annotations', 
-          'instances_extreme_{}2017.json').format(split)
-      else:
-        self.annot_path = os.path.join(
-          self.data_dir, 'annotations', 
-          'instances_{}2017.json').format(split)
-    self.max_objs = 128
-    self.class_name = [
-      '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-      'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
-      'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-      'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
-      'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-      'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-      'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
-      'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-      'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-      'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
-      'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
-      'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-      'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-    self._valid_ids = [
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 
-      14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 
-      24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 
-      37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 
-      48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
-      58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 
-      72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 
-      82, 84, 85, 86, 87, 88, 89, 90]
+    self.max_objs = 2
+    self.class_name = ['__background__', 'L', 'R']
+    # TODO: I hope this corresponds to valid object categories
+    self._valid_ids = [1, 2]
     self.cat_ids = {v: i for i, v in enumerate(self._valid_ids)}
     self.voc_color = [(v // 32 * 64 + 64, (v // 8) % 4 * 64, v % 8 * 32) \
                       for v in range(1, self.num_classes + 1)]
     self._data_rng = np.random.RandomState(123)
+    # TODO: I have no idea what this means and I didn't find usage of it
     self._eig_val = np.array([0.2141788, 0.01817699, 0.00341571],
                              dtype=np.float32)
     self._eig_vec = np.array([
@@ -83,6 +53,7 @@ class SurgAI(data.Dataset):
     self.split = split
     self.opt = opt
 
+    #Gil: they use the variable name "coco' in kitti and pascal as well, I don't know why, but we'll use it here as well
     print('==> initializing coco 2017 {} data.'.format(split))
     self.coco = coco.COCO(self.annot_path)
     self.images = self.coco.getImgIds()
@@ -93,6 +64,7 @@ class SurgAI(data.Dataset):
   def _to_float(self, x):
     return float("{:.2f}".format(x))
 
+  # TODO: I don't think this is needed
   def convert_eval_format(self, all_bboxes):
     # import pdb; pdb.set_trace()
     detections = []
@@ -124,13 +96,15 @@ class SurgAI(data.Dataset):
     json.dump(self.convert_eval_format(results), 
                 open('{}/results.json'.format(save_dir), 'w'))
   
+  # TODO: this function is overloaded in each dataset and used to run evaluation, I will probably need to write my own
   def run_eval(self, results, save_dir):
     # result_json = os.path.join(save_dir, "results.json")
     # detections  = self.convert_eval_format(results)
     # json.dump(detections, open(result_json, "w"))
     self.save_results(results, save_dir)
-    coco_dets = self.coco.loadRes('{}/results.json'.format(save_dir))
-    coco_eval = COCOeval(self.coco, coco_dets, "bbox")
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()
+    #coco_dets = self.coco.loadRes('{}/results.json'.format(save_dir))
+    #coco_eval = COCOeval(self.coco, coco_dets, "bbox")
+    #coco_eval.evaluate()
+    #coco_eval.accumulate()
+    #coco_eval.summarize()
+    return None
